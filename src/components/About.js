2fadf9092b57ff23471be147337e5a45f54d6505
Mtuozzo86 <Mtuozzo86@gmail.com>
2022-11-17 13:58:41 -0500

    better looking hours

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,51 @@ function About() {
   return (
     <div className="about-section">
       <div className="about-container d-flex flex-wrap">
-        {/* <Row className="h-100">
-          <Col className="d-flex h-100 text-warning">
-            <div className="d-flex flex-column about-first p-3">
-              <p className="fs-5 fw-semibold">
+        <div className="about-first p-2">
+          <p className="fs-5 fw-semibold text-center">
+            400 Garden City Plaza Ste 110 <br />
+            Garden City, NY 11530
+          </p>
+          <p className="text-center">Hours</p>
+          <div className="hours">
+            <Row>
+              <Col>Monday</Col>
+              <Col className="text-end">Closed</Col>
+            </Row>
+            <Row>
+                <Col>Tuesday</Col>
+                <Col className="text-end">Closed</Col>
+              </Row>
+              <Row>
+                <Col>Wednesday</Col>
+                <Col className="text-end">9 AM - 8 PM</Col>
+              </Row>
+              <Row>
+                <Col>Thursday</Col>
+                <Col className="text-end">9 AM - 8 PM</Col>
+              </Row>
+              <Row>
+                <Col>Friday</Col>
+                <Col className="text-end">9 AM - 8 PM</Col>
+              </Row>
+              <Row>
+                <Col>Saturday</Col>
+                <Col className="text-end">9 AM - 8 PM</Col>
+              </Row>
+              <Row>
+                <Col>Sunday</Col>
+                <Col className="text-end">Closed</Col>
+              </Row>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
+{
+  /* <p className="fs-5 fw-semibold">
                 400 Garden City Plaza Ste 110, Garden City, NY 11530
               </p>
               <p className="text-center">Hours</p>
@@ -43,16 +84,5 @@ function About() {
               <Row>
                 <Col>Sunday</Col>
                 <Col className="text-end">Closed</Col>
-              </Row>
-            </div>
-          </Col>
-          <Col className="d-flex h-100 text-warning">
-            <div className="about-second"></div>
-          </Col>
-        </Row> */}
-      </div>
-    </div>
-  );
+              </Row> */
 }
-
-export default About;
